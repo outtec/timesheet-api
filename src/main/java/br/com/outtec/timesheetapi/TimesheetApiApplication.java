bb82981035c298161064dd0cce274d7474e00a83
gbessa <gbvirtual@gmail.com>
2018-05-07 11:14:22 -0300

    Project created at STS

---
+++ src/main/java/br/com/outtec/timesheetapi/TimesheetApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.outtec.timesheetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetApiApplication.class, args);
+	}
+}
