3a65723008cd5c541406e35fe6b471560a3ea26e
JoyceAquino <joycesaquino@gmail.com>
2018-05-23 11:49:54 -0300

    PasswordUtils for encrypt password using BCrypt

--- src/main/java/br/com/outtec/timesheetapi/TimesheetApiApplication.java
+++ src/main/java/br/com/outtec/timesheetapi/TimesheetApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import br.com.outtec.utils.PasswordUtils;
+
 
 @SpringBootApplication
 public class TimesheetApiApplication {
@@ -14,11 +16,21 @@ public class TimesheetApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TimesheetApiApplication.class, args);
 	}
-	
+	private static final String PASSWORD = "whereIStheLsS?";
+
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 
+			String encodedPassword = PasswordUtils.getBCrypt(PASSWORD);
+			System.out.println("Password encoded : " + encodedPassword);
+			
+			encodedPassword = PasswordUtils.getBCrypt(PASSWORD);
+			System.out.println("Password encoded hash again : " + encodedPassword);
+			
+			encodedPassword = PasswordUtils.getBCrypt(PASSWORD);
+			System.out.println("Valid Password : " + PasswordUtils.isValidPassword(PASSWORD, encodedPassword));
+			
 			
 		};
 }
