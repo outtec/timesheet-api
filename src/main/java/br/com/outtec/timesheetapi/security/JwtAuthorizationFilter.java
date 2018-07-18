ff686ed5fc1acc35c3f7afa312634004a9353932
joycesaquino <joycesaquino@gmail.com>
2018-07-18 17:29:26 -0300

    Finalizando implementação do novo método de login

--- src/main/java/br/com/outtec/timesheetapi/security/JwtAuthorizationFilter.java
+++ src/main/java/br/com/outtec/timesheetapi/security/JwtAuthorizationFilter.java
@@ -1,12 +1,4 @@
-package br.com.outtec.timesheetapi.security.filters;
-
-import javax.servlet.Filter;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import br.com.outtec.timesheetapi.security.utils.JwtTokenUtil;
-
+package br.com.outtec.timesheetapi.security;
 
 import java.io.IOException;
 
@@ -15,20 +7,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-	private JwtTokenUtil jwtTokenUtil;
+	private JWTUtil jwtUtil;
 	
 	private UserDetailsService userDetailsService;
 	
-	public JwtAuthorizationFilter(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil, UserDetailsService userDetailsService) {
+	public JwtAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UserDetailsService userDetailsService) {
 		super(authenticationManager);
-		this.jwtTokenUtil = jwtTokenUtil;
+		this.jwtUtil = jwtUtil;
 		this.userDetailsService = userDetailsService;
 	}
 	
@@ -48,8 +42,8 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 	}
 
 	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
-		if (jwtTokenUtil.tokenValido(token)) {
-			String username = jwtTokenUtil.getUsernameFromToken(token);
+		if (jwtUtil.tokenValido(token)) {
+			String username = jwtUtil.getUsername(token);
 			UserDetails user = userDetailsService.loadUserByUsername(username);
 			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 		}
