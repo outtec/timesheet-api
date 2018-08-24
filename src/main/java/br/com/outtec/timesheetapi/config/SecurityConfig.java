e389143f8e31951ea8d63a213b2bbc6d65c9b403
joycesaquino <joycesaquino@gmail.com>
2018-08-24 15:27:11 -0300

    Atualizando no heroku com o remote correto

--- src/main/java/br/com/outtec/timesheetapi/config/SecurityConfig.java
+++ src/main/java/br/com/outtec/timesheetapi/config/SecurityConfig.java
@@ -35,10 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTUtil jwtUtil;
 
-	private static final String[] PUBLIC_MATCHERS = {
-			
-	};
-
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/timesheets/**",
 			"/collaborators/**",
@@ -52,11 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	private static final String[] PUBLIC_MATCHERS_DELETE = {
 			"/timesheets/**",
-			"/collaborators/**",
+			"/collaborators/**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
-			"/timesheets/**",
+			"/timesheets/**"
 		};
 
 
@@ -74,7 +70,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
 		.antMatchers(HttpMethod.DELETE, PUBLIC_MATCHERS_DELETE).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		httpSecurity.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
 		httpSecurity.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
