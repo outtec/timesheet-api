52d11545dd8fa6dab4f8d7afdebadd22bf09c9e4
joycesaquino <joycesaquino@gmail.com>
2018-08-31 15:28:25 -0300

    removendo "/auth/forgot**"

--- src/main/java/br/com/outtec/timesheetapi/config/SecurityConfig.java
+++ src/main/java/br/com/outtec/timesheetapi/config/SecurityConfig.java
@@ -37,8 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/collaborators/**",
-			"/auth/forgot**",
-			"/auth/refresh_token**"
+			"/auth/forgot**"
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
